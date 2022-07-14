3e801d391640aed5d19fe498d89551b9febcbe3e
Geottuse <geottuse@Geottuses-MacBook-Pro.local>
2022-07-14 15:00:54 -0400

    ready for testflight

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 test = True
 
 server_url = "0.0.0.0"
-local_url = "10.0.0.60"
+local_url = "192.168.2.107"
 apphost = server_url if test == False else local_url
 
 app = Flask(__name__)
@@ -35,6 +35,10 @@ def cancelSchedule(data):
 
 @socket.on("socket/business/cancelAppointment")
 def cancelAppointment(data):
+	socket.emit("updateNotifications", data, to=data['receiver'])
+
+@socket.on("socket/business/pushAppointments")
+def pushAppointments(data):
 	socket.emit("updateNotifications", data, to=data['receiver'])
 
 # booktime
